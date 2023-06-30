29a2b7e5369bcbc41e3887e99522ca233d7f5f31
ahouston <alexehouston@gmail.com>
2023-06-30 14:00:40 -0500

    update nav css

--- src/Nav/Nav.jsx
+++ src/Nav/Nav.jsx
@@ -1,10 +1,9 @@
-import PropTypes from 'prop-types';
+import PropTypes from "prop-types";
 import { FontAwesomeIcon } from "@fortawesome/react-fontawesome";
 import { faChevronDown } from "@fortawesome/free-solid-svg-icons";
 import "./Nav.css";
 
 export default function Nav({ isSmallScreen }) {
-
   return (
     <div className="Nav animate__animated animate__fadeInDown animate__slow">
       <div className="container-fluid d-flex flex-column justify-content-center align-items-center">
@@ -12,95 +11,117 @@ export default function Nav({ isSmallScreen }) {
           <div className="front"></div>
           <div className="back"></div>
         </div>
-        <ul className="ribbon-nav position-relative d-flex justify-content-around align-items-center px-2">
+        <ul className="ribbon-nav position-relative d-flex justify-content-center align-items-center px-3">
           {isSmallScreen ? (
-            <li className="nav-link lh-1 px-2 text-uppercase">
-              <div className="dropdown">
-                <a
-                  className="underline-animation dropdown-toggle text-white"
-                  href="#"
-                  role="button"
-                  id="navLinksDropdown"
-                  data-bs-toggle="dropdown"
-                  aria-expanded="false"
-                >
-                  Explore <FontAwesomeIcon icon={faChevronDown} className="text-pink" />
+            <>
+              <li className="nav-link lh-1 pe-3 text-uppercase my-3">
+                <div className="dropdown">
+                  <a
+                    className="underline-animation dropdown-toggle text-white"
+                    href="#"
+                    role="button"
+                    id="navLinksDropdown"
+                    data-bs-toggle="dropdown"
+                    aria-expanded="false"
+                  >
+                    Explore{" "}
+                    <FontAwesomeIcon
+                      icon={faChevronDown}
+                      className="text-pink"
+                    />
+                  </a>
+                  <ul
+                    className="dropdown-menu"
+                    aria-labelledby="navLinksDropdown"
+                  >
+                    <li>
+                      <a className="dropdown-item" href="#">
+                        Home
+                      </a>
+                    </li>
+                    <li>
+                      <a className="dropdown-item" href="#">
+                        Practice Areas
+                      </a>
+                    </li>
+                    <li>
+                      <a className="dropdown-item" href="#">
+                        Contact Us
+                      </a>
+                    </li>
+                  </ul>
+                </div>
+              </li>
+              <li className="nav-link text-uppercase">
+                <a href="#" className="underline-animation p-0 my-3 ps-3">
+                  Consultation{" "}
+                  <FontAwesomeIcon icon={faChevronDown} className="text-pink" />
                 </a>
-                <ul className="dropdown-menu" aria-labelledby="navLinksDropdown">
-                  <li>
-                    <a className="dropdown-item" href="#">
-                      Home
-                    </a>
-                  </li>
-                  <li>
-                    <a className="dropdown-item" href="#">
-                      Practice Areas
-                    </a>
-                  </li>
-                  <li>
-                    <a className="dropdown-item" href="#">
-                      Contact Us
-                    </a>
-                  </li>
-                </ul>
-              </div>
-            </li>
+              </li>
+            </>
           ) : (
-            <> 
+            <>
               <li className="nav-link text-uppercase">
                 <a href="#" className="underline-animation p-0 my-2 mx-4">
                   Home
                 </a>
               </li>
               <li className="nav-link text-uppercase">
-              <div className="dropdown underline-animation p-0 my-2 mx-4">
-                <a
-                  className="dropdown-toggle text-white"
-                  href="#"
-                  role="button"
-                  id="navLinksDropdown"
-                  data-bs-toggle="dropdown"
-                  aria-expanded="false"
-                >
-                  Practice Areas <FontAwesomeIcon icon={faChevronDown} className="text-pink" />
-                </a>
-                <ul className="dropdown-menu" aria-labelledby="navLinksDropdown">
-                  <li>
-                    <a className="dropdown-item" href="#">
-                      Personal Injury
-                    </a>
-                  </li>
-                  <li>
-                    <a className="dropdown-item" href="#">
-                      Intellectual Property
-                    </a>
-                  </li>
-                  <li>
-                    <a className="dropdown-item" href="#">
-                      Business Law
-                    </a>
-                  </li>
-                  <li>
-                    <a className="dropdown-item" href="#">
-                      Real Estate Law
-                    </a>
-                  </li>
-                </ul>
-              </div>
+                <div className="dropdown underline-animation p-0 my-2 mx-4">
+                  <a
+                    className="dropdown-toggle text-white"
+                    href="#"
+                    role="button"
+                    id="navLinksDropdown"
+                    data-bs-toggle="dropdown"
+                    aria-expanded="false"
+                  >
+                    Practice Areas{" "}
+                    <FontAwesomeIcon
+                      icon={faChevronDown}
+                      className="text-pink"
+                    />
+                  </a>
+                  <ul
+                    className="dropdown-menu"
+                    aria-labelledby="navLinksDropdown"
+                  >
+                    <li>
+                      <a className="dropdown-item" href="#">
+                        Personal Injury
+                      </a>
+                    </li>
+                    <li>
+                      <a className="dropdown-item" href="#">
+                        Intellectual Property
+                      </a>
+                    </li>
+                    <li>
+                      <a className="dropdown-item" href="#">
+                        Business Law
+                      </a>
+                    </li>
+                    <li>
+                      <a className="dropdown-item" href="#">
+                        Real Estate Law
+                      </a>
+                    </li>
+                  </ul>
+                </div>
               </li>
               <li className="nav-link text-uppercase">
                 <a href="#" className="underline-animation p-0 my-2 mx-4">
                   Contact Us
                 </a>
               </li>
+              <li className="nav-link text-uppercase">
+                <a href="#" className="underline-animation p-0 my-2 mx-4">
+                  Consultation{" "}
+                  <FontAwesomeIcon icon={faChevronDown} className="text-pink" />
+                </a>
+              </li>
             </>
           )}
-          <li className="nav-link text-uppercase">
-            <a href="#" className="underline-animation p-0 my-2 mx-4">
-              Consultation{' '}
-              <FontAwesomeIcon icon={faChevronDown} className="text-pink" />
-            </a>
-          </li>
         </ul>
       </div>
     </div>
